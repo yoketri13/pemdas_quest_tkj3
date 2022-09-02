86b99c9c707a8f34b23ab3e3e9a6af9ba3c4bfb9
yoketri13 <112367345+yoketri13@users.noreply.github.com>
2022-09-02 10:24:32 +0700

    yoke tri nugroho

---
+++ python5-tkj3.py
@@ -0,0 +1,41 @@
+# rumus persegi sisi* sisi
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah", luas_p)
+
+
+# rumus segitiga a* t / 2
+
+alas = 40
+tinggi = 20
+luas_s = alas * tinggi / 2
+
+print("luas segitiga adalah", luas_s)
+
+
+# rumus jajar genjang
+
+alas = 30
+tinggi = 15
+luas_j = alas * tinggi
+
+print("luas jajar genjang adalah", luas_j)
+
+
+# rumus lingkaran
+
+phi = 22/7
+r = 14
+luas_l = phi* r * r
+
+print("luas lingkaran adalah", luas_l)
+
+
+#program dinamis#
+
+print("\nl.luas persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah", lua_p)
