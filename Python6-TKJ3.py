6263a0d193d953deec9289f42aa870c74447aff3
yoketri13 <112367345+yoketri13@users.noreply.github.com>
2022-09-04 22:05:11 -0700

    Add files via upload

---
+++ Python6-TKJ3.py
@@ -0,0 +1,17 @@
+# dolar ke rupiah 
+
+dolar = int(input("masukan nilai dolar: "))
+nilai_r = dolar * 14973.8
+print("nilai 1 dolar adalah", nilai_r)
+
+# euro ke won 
+
+euro = int(input("masukan nilai euro: "))
+nilai_w = euro * 1359.22
+print("nilai 1 euro adalah", nilai_w)
+
+# yen ke rupiah     
+
+yen = int(input("masukan nilai yen: "))
+nilai_r = yen * 106.25
+print("nilai 1 yen adalah", nilai_r)
