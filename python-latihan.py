205cb9d881f5e3eafe629de3eee8e5381f91cd94
yoketri13 <112367345+yoketri13@users.noreply.github.com>
2022-09-07 23:40:36 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,32 @@
+# persegi
+sisi = int(input("masukan nilai sisi"))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+
+# persegi panhjang
+panjang = int(input("masukan nilai panjang"))
+lebar = int(input9"masukan nilai lebar"))
+luas_pj = panjang * lebar
+print("luas persegi panjang adalah", luas_pj)
+
+
+# jajar genjang
+alas = int(input("masukan nilai alas"))
+tinggi = int(input("masukan nilai tinggi"))
+luas_jg = alas * tinggi
+prin("luas jajar genjang adalah", luas_jg)
+
+
+# lingkaran
+r = int(input("masukan nilai r"))
+phi = 22/7
+luas_l = phi * r * r
+print("luas lingkaran adalah", luas_l))
+
+
+# segitiga
+alas = int(input("masukan nilai alas"))
+tinggi = int(input("masukan nilai tinggi"))
+luas_s = 1/2 * alas *tinggi
+print ("luas segitiga adalah", luas_s)
